dbf398159ec2ad48328edbfda5a4ab444c73dc76
ErsaGunTosun <ersagunfatihdev@gmail.com>
2023-08-19 14:09:46 +0300

    changed footer and home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ function Footer() {
         <div className='grid grid-cols-2 text-xs sm:text-sm mb-11'>
             <div className='justify-self-start text-start'>
                 <p className='m-0 p-0 mb-2'>Currently in</p>
-                <p className='m-0 p-0'>🇩🇪 Berlin, Germany</p>
+                <p className='m-0 p-0'>🇹🇷 Turkey</p>
             </div>
             <div className='justify-self-end text-end'>
-                <p className='m-0 p-0 mb-2'>© 2023 Ebru Kaya</p>
-                <a href='mailto:fatihetosun@gmail' className='m-0 p-0 '>ebru[at]ebrukaya.me 📮</a>
+                <p className='m-0 p-0 mb-2'>© 2023 Ersagun Tosun</p>
+                <a href='mailto:ersagun@ersaguntosun.com' className='m-0 p-0 '>ersagun[at]ersaguntosun.com 📮</a>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,24 +16,24 @@ function Home() {
                 <Header islogo={false} ispage={''} />
                 <div className='flex mt-2'>
                     <div className='m-0 p-0 text-justify'>
-                        <h1 className='text-4xl font-bold mb-6'>Hi, I'm Ebru.</h1>
-                        <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Sr. Frontend Engineer at <strong>RoomPriceGenie</strong> 🧞</h3>
+                        <h1 className='text-4xl font-bold mb-6'>Hi, I'm ErsaGun.</h1>
+                        {/* <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Sr. Frontend Engineer at <strong>RoomPriceGenie</strong> 🧞</h3>
                         <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Founder at <strong>Noe Crafts</strong> 🍃</h3>
-                        <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Co-Founder at <strong>Assistive Cards</strong> 🐥</h3>
-                        <p className='my-6 text-lg text-justify sm:pr-9 tracking-tighter'>
+                        <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Co-Founder at <strong>Assistive Cards</strong> 🐥</h3> */}
+                        {/* <p className='my-6 text-lg text-justify sm:pr-9 tracking-tighter'>
                             I have always been so curious about the whole software development cycle, from just an idea to the journey of bringing it to life.
                             Therefore I enjoy spending my time improving myself in full stack development while mostly focusing on the frontend.
-                        </p>
+                        </p> */}
                         <div className='text-2.5xl text-start m-0 mt-4 p-0 inline'>
-                            <a href='#'><i className="fa-brands fa-linkedin-in me-2"></i></a>
-                            <a href='#'><i className="fa-brands fa-github me-2"></i></a>
-                            <a href='#'><i className="fa-brands fa-medium me-2"></i></a>
-                            <a href='#'><i className="fa-brands fa-twitter"></i></a>
+                            <a href='www.linkedin.com/in/fatihetosun'><i className="fa-brands fa-linkedin-in me-2"></i></a>
+                            <a href='https://github.com/ErsaGunTosun'><i className="fa-brands fa-github me-2"></i></a>
+                            {/* <a href='#'><i className="fa-brands fa-medium me-2"></i></a>
+                            <a href='#'><i className="fa-brands fa-twitter"></i></a> */}
                         </div>
                     </div>
-                    <img src={logo} className='hidden sm:block sm:w-64 h-fit' />
+                    {/* <img src={logo} className='hidden sm:block sm:w-64 h-fit' /> */}
                 </div>
-                <hr className='my-10 opacity-20' />
+                {/* <hr className='my-10 opacity-20' />
                 <div className=''>
                     <ArticleCard title="facing this meaninglessness courageously" time="2022-09-21, by ebru" content="Today I finished reading Optimistic Nihilism: A Psychologist's Personal Story, and as always I am in love with a quote. The book itself was…" />
                     <ArticleCard title="optimistic nihilism" time="2022-08-28, by ebru" content="I asked DALL·E to draw me an image for this post saying optimist nihilist enjoying life in mountains, digital art and did an amazing job as…" />
@@ -44,7 +44,7 @@ function Home() {
                         <a href='#' className='font-bold'>1 /</a>
                         <a href='#'> 2 /</a>
                     </p>
-                </div>
+                </div> */}
                 <hr className='my-8 opacity-20' />
                 <Footer />
             </div>
