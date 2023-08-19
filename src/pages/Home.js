9945c6ff10a238fab05231284a124cbf6593c7e2
ErsaGunTosun <ersagunfatihdev@gmail.com>
2023-08-19 15:09:31 +0300

    Finished home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import Footer from '../components/Footer.js';
 import ArticleCard from '../components/ArticleCard.js';
 
 //assets
-import logo from '../assets/images/profil.png'
+import logo from '../assets/images/profil.jpg'
 
 
 function Home() {
@@ -20,31 +20,19 @@ function Home() {
                         {/* <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Sr. Frontend Engineer at <strong>RoomPriceGenie</strong> 🧞</h3>
                         <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Founder at <strong>Noe Crafts</strong> 🍃</h3>
                         <h3 className='text-1.5xl font-semibold my-1 tracking-tight'>Co-Founder at <strong>Assistive Cards</strong> 🐥</h3> */}
-                        {/* <p className='my-6 text-lg text-justify sm:pr-9 tracking-tighter'>
+                        <p className='my-6 text-lg text-justify sm:pr-9 tracking-tighter'>
                             I have always been so curious about the whole software development cycle, from just an idea to the journey of bringing it to life.
-                            Therefore I enjoy spending my time improving myself in full stack development while mostly focusing on the frontend.
-                        </p> */}
+                            Therefore spending my time improving myself in full stack development
+                        </p>
                         <div className='text-2.5xl text-start m-0 mt-4 p-0 inline'>
                             <a href='www.linkedin.com/in/fatihetosun'><i className="fa-brands fa-linkedin-in me-2"></i></a>
                             <a href='https://github.com/ErsaGunTosun'><i className="fa-brands fa-github me-2"></i></a>
-                            {/* <a href='#'><i className="fa-brands fa-medium me-2"></i></a>
-                            <a href='#'><i className="fa-brands fa-twitter"></i></a> */}
+                            <a href='https://dev.to/ersaguntosun'><i className="fa-brands fa-dev me-2"></i></a>
+                            <a href='https://twitter.com/ErsaGunTosun'><i className="fa-brands fa-twitter me-2"></i></a>
                         </div>
                     </div>
-                    {/* <img src={logo} className='hidden sm:block sm:w-64 h-fit' /> */}
+                    <img src={logo} className='hidden sm:block sm:w-64 h-fit rounded-md' />
                 </div>
-                {/* <hr className='my-10 opacity-20' />
-                <div className=''>
-                    <ArticleCard title="facing this meaninglessness courageously" time="2022-09-21, by ebru" content="Today I finished reading Optimistic Nihilism: A Psychologist's Personal Story, and as always I am in love with a quote. The book itself was…" />
-                    <ArticleCard title="optimistic nihilism" time="2022-08-28, by ebru" content="I asked DALL·E to draw me an image for this post saying optimist nihilist enjoying life in mountains, digital art and did an amazing job as…" />
-                    <ArticleCard title="the games" time="2022-08-17, by ebru" content="This week I have been on sick leave and not productive at all since I somehow managed to catch a cold after a lake trip, but there is no…" />
-                    <ArticleCard title="dreamers and realists" time="2020-08-03, by ebru" content="There are dreamers and there are realists in this world Youd think the dreamers would find the dreamers and the realists would find the… " />
-                    <ArticleCard title="the movies" time="2020-04-26, by ebru" content="How the quarantine is going? I have recently been watching a lot of movies. I started a list at the beginning of this year, I was going to…" />
-                    <p className='text-lg'>
-                        <a href='#' className='font-bold'>1 /</a>
-                        <a href='#'> 2 /</a>
-                    </p>
-                </div> */}
                 <hr className='my-8 opacity-20' />
                 <Footer />
             </div>
