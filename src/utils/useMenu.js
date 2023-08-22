a2c133f5a2ed772c5d880997c6f04490d39f3202
ErsaGunTosun <ersagunfatihdev@gmail.com>
2023-08-22 17:15:04 +0300

    safari browser trying bug fix

--- src/utils/useMenu.js
+++ src/utils/useMenu.js
@@ -10,8 +10,21 @@ export default function useMenu() {
     useEffect(() => {
         if(isMenuOpen){
             document.body.style.overflow = 'hidden';
+            document.body.style.position = 'fixed';
+            document.body.style.top = 0;
+            document.body.style.left = 0;
+            document.body.style.right = 0;
+            document.body.style.bottom = 0;
+
+
         }else{
             document.body.style.overflow = 'unset';
+            document.body.style.position = 'unset';
+            document.body.style.top = 'unset';
+            document.body.style.left = 'unset';
+            document.body.style.right = 'unset';
+            document.body.style.bottom = 'unset';
+            
         }
     }, [isMenuOpen])
 
